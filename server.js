a4beea9bbf2330152741f7918472f740df805979
hgarciahumberto-ctrl <hgarcia.humberto@gmail.com>
2026-03-18 14:33:13 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const { twiml: { VoiceResponse } } = require("twilio");
+import express from "express";
+import bodyParser from "body-parser";
+import twilio from "twilio";
+
+const { twiml: { VoiceResponse } } = twilio;
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
